8f12ed1fdb54d9a2e21833eb46cbbf5e59aa3007
Mikey <80923101+skyyriiz@users.noreply.github.com>
2023-06-01 19:23:03 +0200

    Update silent_install.bat

--- silent_install.bat
+++ silent_install.bat
@@ -4,3 +4,5 @@ powershell.exe -command "curl -O https://www.python.org/ftp/python/3.9.9/python-
 
 powershell.exe -command "python-3.9.9-amd64.exe /quiet InstallAllUsers=1 PrependPath=1"
 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
