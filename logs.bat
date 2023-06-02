3373984a9446e34ecee95fa9e8137ec1d2c5353a
Goenae <61237031+Goenae@users.noreply.github.com>
2023-06-02 20:18:30 +0200

    Update logs.bat

--- logs.bat
+++ logs.bat
@@ -1,4 +1,4 @@
 @echo off
 REM SLEEP 20
 REM type NUL > EmptyFile.txt
-Powershell.exe -NoLogo -WindowStyle Hidden -executionpolicy remotesigned -File  truc.ps1
+Powershell.exe -NoLogo -WindowStyle Hidden -executionpolicy remotesigned -File  logs.ps1
