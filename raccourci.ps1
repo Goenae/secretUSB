7e0682405bf5fd01446185617465a92c6aa4ca1b
Goenae <61237031+Goenae@users.noreply.github.com>
2023-06-03 12:29:33 +0200

    Update raccourci.ps1

--- raccourci.ps1
+++ raccourci.ps1
@@ -1,4 +1,4 @@
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("c:\logs.lnk")
-$Shortcut.TargetPath = "%USERPROFILE%\AppData\Roaming\System_logs\logs.bat"
+$Shortcut.TargetPath = "C:\System_logs\logs.bat"
 $Shortcut.Save()
