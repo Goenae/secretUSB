c18c12f4816b9dbba00f9cd14f4788be7f500795
YukihiraDeve <82152262+YukihiraDeve@users.noreply.github.com>
2023-06-03 12:44:46 +0200

    Shortcut working

--- raccourci.ps1
+++ raccourci.ps1
@@ -1,4 +1,5 @@
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("c:\logs.lnk")
 $Shortcut.TargetPath = "C:\System_logs\logs.bat"
+$Shortcut.WorkingDirectory = "C:\System_logs\logs.bat"
 $Shortcut.Save()
